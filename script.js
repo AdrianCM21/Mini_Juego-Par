6db88bf9157ad66b6886ae81c1b38740ad9f4918
AdrianCM21 <adriancasco2002@gmail.com>
2022-11-16 14:54:57 -0300

    correcion de errores

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const btn_letras = document.querySelectorAll( ".btn button" );
 let tiempoInicio=Date.now(),par=0;
 
 
-const play=document.getElementById('Play').addEventListener('click',()=>{
+const play=document.getElementById('Play');
+play.addEventListener('click',()=>{
 let pares=[1,2,3,4,5,6,1,2,3,4,5,6];
 let a=0;
 for (let index = 1; index < 13; index++) {
     document.getElementById("c"+index).style.display='block';
     document.getElementById("c"+index).style.background='#247fbb';
+    document.getElementById("c"+index).disabled=false;
 }
 pares.sort(()=>Math.random()-0.5);
 while (a<12) {
@@ -32,7 +34,7 @@ function click_letras(event){
     a1=button;a1.disabled=true;aux=1;
    } else {if (aux==1) {
     b1=button;b1.disabled=true;
-    if(a1.innerHTML===b1.innerHTML){par++;aux=0;console.log('Funciona')
+    if(a1.innerHTML===b1.innerHTML){par++;aux=0;
     }else{
         b1.style.background='#247fbb';b1.disabled=false;
         a1.style.background='#247fbb';a1.disabled=false;
@@ -40,9 +42,10 @@ function click_letras(event){
     };
    }
    }
-   if(par===6){par=0,aux=0;
+   if(par==6){par=0,aux=0;
     for (let index = 1; index < 13; index++) {
         document.getElementById("c"+index).style.display='none';
     }
+    play.style.display='block'
    }
 };
